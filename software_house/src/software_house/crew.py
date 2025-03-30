8c21c5403688af7bda1ed1fb8375f3ab121fe258
Walxiney <51864929+Walxiney@users.noreply.github.com>
2025-03-30 14:33:37 +0100

    Add files via upload

--- software_house/src/software_house/crew.py
+++ software_house/src/software_house/crew.py
@@ -1,120 +1,150 @@
-#from crewai.project import CrewBase, agent, crew, task, before_kickoff, after_kickoff
-from crewai import Flow
-from crewai.flow.flow import listen, start, and_
+from crewai import Agent, Crew, Process, Task
 import yaml
-import json
+import os
 
-from agent_tasks_crew import AgentsTasksCrew
+class AgentsTasksCrew():
+    """Agents and Tasks crew
+    This class is responsible for creating a crew of agents and tasks based on the provided YAML configurations.
+    It includes methods to read agent and task configurations from YAML files, create a crew orchestrator,
+    and dynamically create agents and tasks based on the provided team configuration.
+    """
 
-from dotenv import load_dotenv
-load_dotenv()
+    def __init__(self):
+        pass
 
-class SoftwareHouse(Flow):
-	"""SoftwareHouse flow"""
-	@start()
-	def project_description(self):
-		"""
-		Describe the project.
-		"""
-		file_path = 'software_house/src/software_house/config/input.yaml'
-		with open(file_path, 'r', encoding='utf-8') as file:
-			project_yml = yaml.safe_load(file)
-			project_info = project_yml["project"]
-		return project_info
-	
-	@listen(project_description)
-	def available_agents(self):
-		"""
-		Load the available agents and tasks from the agents.yaml and tasks.yaml files.
-		"""
-		# Load the available agents from the agents.yaml file
-		agents_file_path = 'software_house/src/software_house/config/agents_few.yaml'
-		with open(agents_file_path, 'r', encoding='utf-8') as file:
-			agents_data = yaml.safe_load(file)
+    def read_agents_tasks(self)-> dict:
+        """
+        Read agents and tasks from YAML files.
+        This method loads the configurations for agents and tasks from the specified YAML files.
+        - input: None
+        - output: Dictionary containing agents and tasks configurations
+        """
+        # Define file paths for YAML configurations
+        files = {
+            'agents': 'software_house/src/software_house/config/agents_few.yaml',
+            'tasks': 'software_house/src/software_house/config/tasks_few.yaml',
+        }
 
-		# Load the available tasks from the tasks.yaml file
-		tasks_file_path = 'software_house/src/software_house/config/tasks_few.yaml'
-		with open(tasks_file_path, 'r', encoding='utf-8') as tasks_file:
-			tasks_data = yaml.safe_load(tasks_file)
+        # Load configurations from YAML files
+        configs = {}
+        
+        for config_type, file_path in files.items():
+            with open(file_path, 'r', encoding='utf-8') as file:
+                configs[config_type] = yaml.safe_load(file)
 
-		# Prepare the output in the format required for creating_crew_dev
-		agents_tasks = {}
-		for task_name, task_details in tasks_data.items():
-			agent_name = task_details.get('agent')
-			if agent_name in agents_data:
-				agents_tasks[agent_name] = task_name
+        return configs
 
-		# return a dictionary with the available agents and tasks
-		agents_tasks_output = {"agents_tasks": agents_tasks}
-		return agents_tasks_output
+    def crew_orchestrator(self)-> Crew:
+        """
+        Create the crew orchestrator.
+        This orchestrator is responsible for managing the agents and tasks.
+        - input: None
+        - output: Crew object
+        """
 
-	@listen(and_(project_description, available_agents))
-	def project_info_and_agents_tasks(self, project_info, agents_tasks_output):
-		"""
-		Combine the project information and available agents/tasks.
-		"""
-		# project_info = self.project_description()
-		# agents_tasks_output = self.available_agents()
-		input_data = project_info[0] | agents_tasks_output
-		return input_data
+        try:
+            # Load the available agents from the agents.yaml file
+            agents_file_path = 'software_house/src/software_house/config/agent_orchestrator.yaml'
+            with open(agents_file_path, 'r', encoding='utf-8') as file:
+                agent_orchestrator = yaml.safe_load(file)
+        except FileNotFoundError as e:
+            print(f"Error: {e}. Please check the file path for the agent_orchestrator.yaml.")
 
-	@listen(project_info_and_agents_tasks)
-	def staff_selector(self, input_data):
-		"""
-		Select the staff for the project.
-		"""
-		atc = AgentsTasksCrew()
-		staff_selected = atc.crew_orchestrator()
+        try:
+            # Load the available tasks from the tasks.yaml file
+            tasks_file_path = 'software_house/src/software_house/config/task_orchestrator.yaml'
+            with open(tasks_file_path, 'r', encoding='utf-8') as tasks_file:
+                task_orchestrator = yaml.safe_load(tasks_file)
+        except FileNotFoundError as e:
+            print(f"Error: {e}. Please check the file path for the task_orchestrator.yaml.")
 
-		# run the crew
-		selected_team = {}
-		selected_team = staff_selected.kickoff(input_data)
+        # AGENTS
+        team_selector_agent = Agent(
+            config=agent_orchestrator['team_selector_agent'],
+            llm="gpt-4o-mini",
+            verbose=True
+        )
 
-		self.state["staff_results"] = selected_team
+        # TASKS
+        team_selection_task = Task(
+            config=task_orchestrator['team_selection_task'],
+            agent=team_selector_agent
+            # output_pydantic=AgentTask
+        )
 
-		selected_team_dict = selected_team.raw
-		selected_team_json = json.loads(selected_team_dict.encode('utf-8').decode('utf-8'))
-		return selected_team_json
+        return Crew(
+            agents=[team_selector_agent],
+            tasks=[team_selection_task],
+            verbose=True
+        )
 
-	@listen(and_(project_description, staff_selector))
-	def sh_start(self, project_info, selected_team_json):
-		"""
-		Create the crew for the project.
-		"""
-		atc = AgentsTasksCrew()
-		software_house_team = atc.creating_crew_dev(selected_team_json)
+    def creating_crew_dev(self, team, folder_name)-> Crew:
+        """
+        Create the crew for the project.
+        This method dynamically creates agents and tasks based on the provided team configuration.
+        - input: team (dict) - Dictionary containing the team configuration
+        - output: Crew object
+        """
 
-		# run the crew
-		dev_code = software_house_team.kickoff_for_each(project_info)
-		self.state["dev_results"] = dev_code
-		return dev_code
+        # Load configurations from YAML files
+        configs = self.read_agents_tasks()
 
-if __name__ == "__main__":
-	flow = SoftwareHouse()
-	flow.plot()
-	project_details = flow.project_description()
-	# print("#"*50)
-	# print(project_details[0])
-	# print(type(project_details[0]))
-	# print("#"*50)
-	agents_tasks_result = flow.available_agents()
-	# print("#"*50)
-	# print(agents_tasks_result)
-	# print(type(agents_tasks_result))
-	# print("#"*50)
-	input_data = flow.project_info_and_agents_tasks(project_details, agents_tasks_result)
-	# print("#"*50)
-	# print(input_data)
-	# print("#"*50)
-	team = flow.staff_selector(input_data)
-	print("#"*50)
-	print(team)
-	print("#"*50)
-	code = flow.sh_start(project_details, team)
-	# software = flow.kickoff()
+        # Assign loaded configurations to specific variables
+        agents_config = configs['agents']
+        tasks_config = configs['tasks']
 
-	# sh = SoftwareHouse()
-	# project_details = sh.project_description()
-	# staff_agents_available = sh.available_agents()
-	# staff = sh.staff_selector(project_details, staff_agents_available)
-	# sh.sh_start(project_details, staff)
+        model_to_use = "gpt-4o-mini"
+
+        try:
+            # Dynamically create agents based on the team
+            agents = {}
+            for agent_key in team["agents_tasks"].keys():
+                if agent_key in agents_config:
+                    agents[agent_key] = Agent(
+                        config=agents_config[agent_key],
+                        llm=model_to_use,
+                        verbose=True
+                    )
+        except Exception as e:
+            print(f"Error while creating agents: {e}")
+
+        try:
+            # Dynamically create tasks based on the agents
+            tasks = []
+            output_dir = f"software_house/src/software_house/{folder_name}"
+            # Ensure the output directory exists
+            os.makedirs(output_dir, exist_ok=True)
+
+            for agent_key, task_key in team["agents_tasks"].items():
+                if agent_key in agents and task_key in tasks_config:
+
+                    # Define the output file path
+                    output_file_path = os.path.join(output_dir, f"{task_key}.txt")
+                    # Create or overwrite the file (ensure it's ready for writing)
+                    with open(output_file_path, "w", encoding="utf-8") as file:
+                        file.write("")  # Write an empty string to initialize or clear the file
+                    
+                    task = Task(
+                        config=tasks_config[task_key],
+                        agent=agents[agent_key],
+                        output_file=output_file_path,
+                        context=None,  # Initialize context as None
+                    )
+                    tasks.append(task)
+
+                    t = len(tasks)
+                    # Set the context for the code aggregation task
+                    if task_key == "code_aggregation_task" and t >= 2:
+                        t = len(tasks)
+                        tasks[-1].context = [tasks[t-3], tasks[t-2]]
+                    
+        except Exception as e:
+            print(f"Error while creating tasks: {e}")
+
+        # Return the dynamically created crew
+        return Crew(
+            agents=list(agents.values()),
+            tasks=tasks,
+            process= Process.sequential, #Process.hierarchical,
+            verbose=True
+        )
